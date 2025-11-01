b5ccd9f96761b04e4775a7c24072105ee0bb151f
khalii21 <miratkhalikov007@gmail.com>
2025-11-01 13:08:39 +0300

    func make is added

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
-int **make(int r, int c);
+void rm(int **mtx, int r);
+int **make(int r, int c)
+{
+int **mtx = new int *[r];
+for (size_t i = 0; i < r; ++i)
+{
+	try 
+	{
+		mtx[i] = new int[c];
+	}
+	catch(const std::bad_alloc&)
+	{
+		rm(mtx,i);
+		throw;
+	}
+}
+return mtx;
+}
 void output(const int * const *mtx);
 void rm(int **mtx, int r)
 {
@@ -19,7 +36,14 @@ if (std::cin.fail())
 	return 1;
 }
 int **mtx = nullptr;
+try
+{
 mtx = make(rows, cols);
+}
+catch (const std::bad_alloc&)
+{
+return 2;
+}
 output(mtx);
-rm(mtx);
+rm(mtx, rows);
 }
