789df64c417345490c2d43b38e2d18438f495bd3
khalii21 <miratkhalikov007@gmail.com>
2025-11-01 19:38:58 +0300

    final ver klassnoi raboti

--- main.cpp
+++ main.cpp
@@ -1,49 +1,82 @@
 #include <iostream>
 void rm(int **mtx, int r);
+void output(const int * const *mtx, int r, int c);
+void input(int **mtx, int r, int c);
+
 int **make(int r, int c)
 {
-int **mtx = new int *[r];
-for (size_t i = 0; i < r; ++i)
-{
-	try 
+	int **mtx = new int *[r];
+	for (size_t i = 0; i < r; ++i)
 	{
-		mtx[i] = new int[c];
+		try 
+		{
+			mtx[i] = new int[c]; //new int[lns[i]]
+		}
+		catch(const std::bad_alloc &)
+		{
+			rm(mtx,i);
+			throw;
+		}
 	}
-	catch(const std::bad_alloc&)
-	{
-		rm(mtx,i);
-		throw;
-	}
+	return mtx;
 }
-return mtx;
-}
-void output(const int * const *mtx);
+
 void rm(int **mtx, int r)
 {
-for (size_t i = 0; i < r; ++i) 
+	for (size_t i = 0; i < r; ++i) 
+	{
+		delete[] mtx[i];
+	}
+	delete[] mtx;
+}
+
+void input(int **mtx, int r, int c)
 {
-	delete[] mtx[i];
+	for (size_t i = 0; i < r; ++i)
+	{
+		for (size_t j = 0; j < c; ++j)
+		{
+			std::cin >> mtx[i][j];
+		}
+	}
 }
-delete[] mtx;
+
+void output(const int * const *mtx, int r, int c)
+{
+	for (size_t i = 0; i < r; ++i)
+	{
+		for (size_t j = 0; j < c; ++j)
+		{
+			std::cout << mtx[i][j] << " ";	
+		}
+		std::cout << "\n";
+	}
 }
+
 int main()
 {
-int rows = 0;
-int cols = 0;
-std::cin >> rows >> cols;
-if (std::cin.fail())
-{
-	return 1;
-}
-int **mtx = nullptr;
-try
-{
-mtx = make(rows, cols);
-}
-catch (const std::bad_alloc&)
-{
-return 2;
-}
-output(mtx);
-rm(mtx, rows);
+	int rows = 0;
+	int cols = 0;
+	std::cin >> rows >> cols;
+	if (std::cin.fail())
+	{
+		return 1;
+	}
+	int **mtx = nullptr;
+	try
+	{
+		mtx = make(rows, cols);
+	}
+	catch (const std::bad_alloc&)
+	{
+		return 2;
+	}
+	input(mtx, rows, cols);
+	if (std::cin.fail())
+	{
+		rm(mtx, rows);
+		return 1;
+	}
+	output(mtx, rows, cols);
+	rm(mtx, rows);
 }
