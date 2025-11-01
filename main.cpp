fa55d898618f96fed495d1c0fee3915657528a09
khalii21 <miratkhalikov007@gmail.com>
2025-11-01 12:26:34 +0300

    body main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,15 @@
+#include <iostream>
+int **make(int r, int c);
 int main()
 {
+int rows = 0;
+int cols = 0;
+std::cin >> rows >> cols;
+if (std::cin.fail())
+{
+	return 1;
+}
+int **mtx = nullptr;
+mtx = make(rows, cols); 
 }
 
